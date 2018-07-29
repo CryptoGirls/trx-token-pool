56e121349dc410422e5162fb9e4c8b7359a164a2
Irina Iftimie <32229393+puccca@users.noreply.github.com>
2018-07-30 00:00:07 +0300

    Add files via upload

---
+++ votes.js
@@ -0,0 +1,43 @@
+const xhr = require('axios')
+const fs = require('fs');
+
+const CANDIDATE_ADDRESS = 'TVMP5r12ymtNerq5KB4E8zAgLDmg2FqsEG';
+
+// file
+const outFile = 'voters.json';
+
+async function loadVotes() {
+  let start = 0;
+  let results = [];
+
+  while (true) {
+    console.log("start: " + start);
+    let {data} = await xhr.get("https://api.tronscan.org/api/vote", {
+      params: {
+        candidate: CANDIDATE_ADDRESS,
+        limit: 100,
+        sort: 'timestamp',
+        start,
+      }
+    });
+
+    console.log("votes: " + data.data.length)
+
+    if (data.data.length === 0) {
+      break;
+    }
+
+    start += 100;
+
+    results = [...results, ...data.data];
+  }
+
+  return  results ;
+}
+
+loadVotes().then(votes => {
+
+  fs.writeFile(outFile, '{"data":' + JSON.stringify(votes) + "}", 'utf8', () => {
+    console.log('done');
+  });
+})
